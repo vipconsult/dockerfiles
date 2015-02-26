025515afa18776e9a0b1bef8f25865f93b594000
root <root@debian>
2015-02-26 22:50:32 +0000

    Setup logrotate in prod script

--- start_prod.sh
+++ start_prod.sh
@@ -1,6 +1,7 @@
 # /bin/bash
 docker rm -f $(docker ps -aq)
 
+
 docker run --restart=always -d --name mysql1 -p 127.0.0.1:3306:3306 -v /home/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=aaaa -d vipconsult/mysql
 
 docker run --restart=always -d --name pgsql1 -p 127.0.0.1:5432:5432 -v /home/postgresql:/var/lib/postgresql/data -e PG_LOCALE="en_GB.UTF-8 UTF-8" vipconsult/pgsql93
@@ -18,3 +19,5 @@ sudo mkdir -p /home/proftpd
 sudo touch /home/proftpd/ftpd.passwd
 
 docker run --restart=always --net=host -d --name proftpd -v /home/http:/home/ -v /home/proftpd/ftpd.passwd:/etc/proftpd/ftpd.passwd vipconsult/proftpd
+sleep 5
+docker run --restart=always -d -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker michaloo/logrotate
