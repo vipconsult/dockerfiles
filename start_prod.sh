40d728da05b53997bf9309890faaf7c92cab12aa
root <root@debian>
2015-03-03 23:46:07 +0000

    Add the cron image

--- start_prod.sh
+++ start_prod.sh
@@ -6,9 +6,9 @@ docker run --restart=always -d --name mysql1 -p 127.0.0.1:3306:3306 -v /home/mys
 
 docker run --restart=always -d --name pgsql1 -p 127.0.0.1:5432:5432 -v /home/postgresql:/var/lib/postgresql/data -e PG_LOCALE="en_GB.UTF-8 UTF-8" vipconsult/pgsql93
 
-docker run --restart=always -d  -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link pgsql1:pgsql1 vipconsult/php53
+docker run --restart=always -d --name php53 -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link pgsql1:pgsql1 vipconsult/php53
 
-docker run --restart=always -d  -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link pgsql1:pgsql1 vipconsult/php
+docker run --restart=always -d --name php -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link pgsql1:pgsql1 vipconsult/php
 
 docker run --restart=always -d -v /home/simplehelp:/home/simplehelp  --net=host vipconsult/simplehelp
 
