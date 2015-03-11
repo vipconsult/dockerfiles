68df86a4823bd1d15a1837f190358f8356f2b8a0
root <root@debian>
2015-03-11 14:22:31 +0000

    Rename the pgsql container to psql1

--- start_dev.sh
+++ start_dev.sh
@@ -3,11 +3,11 @@ docker rm -f $(docker ps -aq)
 
 docker run --restart=always -d --name mysql1 -p 127.0.0.1:3306:3306 -v /home/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=aaaa -d vipconsult/mysql
 
-docker run --restart=always -d --name pgsql1 -p 127.0.0.1:5432:5432 -v /home/postgresql:/var/lib/postgresql/data -e PG_LOCALE="en_GB.UTF-8 UTF-8" vipconsult/pgsql93
+docker run --restart=always -d --name psql1 -p 127.0.0.1:5432:5432 -v /home/postgresql:/var/lib/postgresql/data -e PG_LOCALE="en_GB.UTF-8 UTF-8" vipconsult/pgsql93
 
-docker run --restart=always -d  -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link pgsql1:pgsql1 vipconsult/php53
+docker run --restart=always -d  -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link psql1:psql1 vipconsult/php53
 
-docker run --restart=always -d  -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link pgsql1:pgsql1 vipconsult/php
+docker run --restart=always -d  -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link psql1:psql1 vipconsult/php
 
 docker run --restart=always -d -v /home/http:/home/http --name nginx -v /var/run:/var/run -p 80:80 -p 443:443  vipconsult/nginx-pagespeed nginx -c /home/http/default/main.conf -g "daemon off;"
 
--- start_prod.sh
+++ start_prod.sh
@@ -3,11 +3,11 @@ docker rm -f $(docker ps -aq)
 
 docker run --name mysql1 --restart=always -d -p 127.0.0.1:3306:3306 -v /home/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=aaaa -d vipconsult/mysql
 
-docker run --name pgsql1 --restart=always -d -p 127.0.0.1:5432:5432 -v /home/postgresql:/var/lib/postgresql/data -e PG_LOCALE="en_GB.UTF-8 UTF-8" vipconsult/pgsql93
+docker run --name psql1 --restart=always -d -p 127.0.0.1:5432:5432 -v /home/postgresql:/var/lib/postgresql/data -e PG_LOCALE="en_GB.UTF-8 UTF-8" vipconsult/pgsql93
 
-docker run --name php53 --restart=always -d -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link pgsql1:pgsql1 vipconsult/php53
+docker run --name php53 --restart=always -d -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link psql1:psql1 vipconsult/php53
 
-docker run --name php --restart=always -d -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link pgsql1:pgsql1 vipconsult/php
+docker run --name php --restart=always -d -v /var/run:/var/run -v /home/http:/home/http  --link mysql1:mysql1  --link psql1:psql1 vipconsult/php
 
 docker run --name simplehelp --restart=always -d -v /home/simplehelp:/home/simplehelp  --net=host vipconsult/simplehelp
 
@@ -22,3 +22,4 @@ sudo touch /home/proftpd/ftpd.passwd
 docker run --restart=always --net=host -d --name proftpd -v /home/http:/home/ -v /home/proftpd/ftpd.passwd:/etc/proftpd/ftpd.passwd vipconsult/proftpd
 sleep 5
 docker run --restart=always -d -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker michaloo/logrotate
+
