54f48548ae95d46a1e255fb257fc91751a3555af
Krasimir Georgiev <support@vip-consult.co.uk>
2015-04-10 20:33:31 +0000

    now the start scripts accept parameters to restart a given container by name

--- start_dev.sh
+++ start_dev.sh
@@ -1,54 +1,92 @@
 # /bin/bash
-docker rm -f $(docker ps -aq)
 
-docker run --restart=always -d --name mysql1 \
-	-p 127.0.0.1:3306:3306 \
-	-v /home/mysql:/var/lib/mysql \
-	-e MYSQL_ROOT_PASSWORD=aaaa \
-	vipconsult/mysql
+if [ "$1" == "" ]; then
+	echo "no container name given so restarting all conatiners"
+	docker rm -f $(docker ps -aq)
+else
+	echo "Removing $1"
+	docker rm -f $1
+fi
 
-docker run --restart=always -d --name psql1 \
-	-p 127.0.0.1:5432:5432 \
-	-v /home/postgresql:/var/lib/postgresql/data \
-	-e PG_LOCALE="en_GB.UTF-8 UTF-8" \
-	vipconsult/pgsql93
+cn="mysql1"
+if [ "$1" == "" ] || [ $1 == $cn ] ;then
+echo "Starting $cn"
+	docker run --restart=always -d --name $cn \
+		-p 127.0.0.1:3306:3306 \
+		-v /home/mysql:/var/lib/mysql \
+		-e MYSQL_ROOT_PASSWORD=aaaa \
+		vipconsult/mysql
+fi
 
-docker run --restart=always -d --name php53 \
-	-v /var/run:/var/run \
-	-v /home/http:/home/http  \
-	--link mysql1:mysql1  \
-	--link psql1:psql1 \
-	-h dev.vip-consult.co.uk \
-	vipconsult/php53
+cn="psql1"
+if [ "$1" == "" ] || [ $1 == $cn ] ;then
+echo "Starting $cn"
+	docker run --restart=always -d --name $cn \
+		-p 127.0.0.1:5432:5432 \
+		-v /home/postgresql:/var/lib/postgresql/data \
+		-e PG_LOCALE="en_GB.UTF-8 UTF-8" \
+		vipconsult/pgsql93
+fi
 
-docker run --restart=always -d  --name php \
-	-v /var/run:/var/run \
-	-v /home/http:/home/http  \
-	--link mysql1:mysql1  \
-	--link psql1:psql1 \
-        -h dev.vip-consult.co.uk \
-	vipconsult/php
+cn="php53"
+if [ "$1" == "" ] || [ $1 == $cn ] ;then
+echo "Starting $cn"
+	docker run --restart=always -d --name $cn \
+		-v /var/run:/var/run \
+		-v /home/http:/home/http  \
+		--link mysql1:mysql1  \
+		--link psql1:psql1 \
+		-h dev.vip-consult.co.uk \
+		vipconsult/php53
+fi
 
-docker run --restart=always -d --name nginx-pagespeed  \
-	-v /home/http:/home/http \
-	-v /var/run:/var/run \
-	-p 80:80 \
-	-p 443:443  \
-	vipconsult/nginx-pagespeed nginx -c /home/http/default/main.conf -g "daemon off;"
+cn="php"
+if [ "$1" == "" ] || [ $1 == $cn ] ;then
+echo "Starting $cn"
+	docker run --restart=always -d  --name $cn \
+		-v /var/run:/var/run \
+		-v /home/http:/home/http  \
+		--link mysql1:mysql1  \
+		--link psql1:psql1 \
+        	-h dev.vip-consult.co.uk \
+		vipconsult/php
+fi
 
-docker run --restart=always -d --name logrotate \
-        -v /var/lib/docker:/var/lib/docker \
-        vipconsult/logrotate
+cn="nginx"
+if [ "$1" == "" ] || [ $1 == $cn ] ;then
+echo "Starting $cn"
+	docker run --restart=always -d --name $cn  \
+		-v /home/http:/home/http \
+		-v /var/run:/var/run \
+		-p 80:80 \
+		-p 443:443  \
+		vipconsult/nginx-pagespeed nginx -c /home/http/default/main.conf -g "daemon off;"
+fi
 
+cn="logrotate"
+if [ "$1" == "" ] || [ $1 == $cn ] ;then
+echo "Starting $cn"
+	docker run --restart=always -d --name $cn \
+        	-v /var/lib/docker:/var/lib/docker \
+        	vipconsult/logrotate
+fi
 
-docker run -v /home:/home --name data \
-	library/debian:wheezy /bin/bash
-sleep 6
+cn="data"
+if [ "$1" == "" ] || [ $1 == $cn ] ;then
+echo "Starting $cn"
+	docker run -v /home:/home --name $cn \
+		library/debian:wheezy /bin/bash
+	sleep 6
+fi
 
-docker run \
-	-v $(which docker):/docker \
-	-v /var/run/docker.sock:/docker.sock \
-	-e USER=vipconsult \
-	-e GROUP=www-data \
-	-e USERID=1002 \
-	vipconsult/samba data
+cn="samba-server"
+if [ "$1" == "" ] || [ $1 == $cn ] ;then
+echo "Starting $cn"
+	docker run \
+		-v $(which docker):/docker \
+		-v /var/run/docker.sock:/docker.sock \
+		-e USER=vipconsult \
+		-e GROUP=www-data \
+		-e USERID=1002 \
+		vipconsult/samba data
+fi
