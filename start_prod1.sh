2a01d5da6a98bc37fff5509e5c88893b66ae979d
Krasimir Georgiev <support@vip-consult.co.uk>
2015-04-10 20:52:23 +0000

    add the startup command in the startup script for freeswitch

--- start_prod1.sh
+++ start_prod1.sh
@@ -1,19 +1,22 @@
 # /bin/bash
 docker rm -f $(docker ps -aq)
 
-docker run --name mysql1 --restart=always -d  \
+docker run --name mysql1 \
+	--restart=always -d  \
 	-p 127.0.0.1:3306:3306 \
 	-v /home/mysql:/var/lib/mysql \
 	-e MYSQL_ROOT_PASSWORD=aaaa  \
 	vipconsult/mysql
 
-docker run --name psql1 --restart=always -d \
+docker run --name psql1 \
+	--restart=always -d \
 	-p 127.0.0.1:5432:5432 \
 	-v /home/postgresql:/var/lib/postgresql/data \
 	-e PG_LOCALE="en_GB.UTF-8 UTF-8" \
 	vipconsult/pgsql93
 
-docker run --name php53 --restart=always -d \
+docker run --name php53 \
+	--restart=always -d \
 	-v /var/run:/var/run \
 	-v /home/http:/home/http  \
 	--link mysql1:mysql1  \
@@ -21,7 +24,8 @@ docker run --name php53 --restart=always -d \
 	-h vip-consult.co.uk \
 	vipconsult/php53
 
-docker run --name php --restart=always -d \
+docker run --name php \
+	--restart=always -d \
 	-v /var/run:/var/run \
 	-v /home/http:/home/http  \
 	--link mysql1:mysql1  \
@@ -29,12 +33,14 @@ docker run --name php --restart=always -d \
 	-h vip-consult.co.uk \
 	vipconsult/php
 
-docker run --name simplehelp --restart=always -d \
+docker run --name simplehelp \
+	--restart=always -d \
 	-v /home/simplehelp:/home/simplehelp  \
 	--net=host \
 	vipconsult/simplehelp
 
-docker run --name nginx --restart=always -d \
+docker run --name nginx \
+	--restart=always -d \
 	-v /home/http:/home/http \
 	-v /var/run:/var/run \
 	-p 178.79.150.62:80:80 \
@@ -47,17 +53,25 @@ docker run --name cron --restart=always -d \
 	-v /home/cron/cron.daily:/etc/cron.daily \
 	vipconsult/cron
 
+sudo docker run --name fs \
+        --restart=always -d \
+        -v /home/freeswitch/sounds:/usr/local/freeswitch/sounds \
+        -v /home/freeswitch/conf:/usr/local/freeswitch/conf \
+        --net=host \
+        vipconsult/freeswitch
+
 sudo rmdir /home/proftpd/ftpd.passwd
 sudo mkdir -p /home/proftpd
 sudo touch /home/proftpd/ftpd.passwd
 
-docker run --name proftpd --restart=always -d \
+docker run --name proftpd \
+	--restart=always -d \
 	--net=host \
-	-v /home/http:/home/ \
+	-v /home:/home \
 	-v /home/proftpd/ftpd.passwd:/etc/proftpd/ftpd.passwd \
 	vipconsult/proftpd
 
-docker run --restart=always -d --name logrotate \
+docker run --name logrotate \
+	--restart=always -d \
 	-v /var/lib/docker:/var/lib/docker \
 	vipconsult/logrotate
-
