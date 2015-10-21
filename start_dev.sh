9ad58bc0f496e3d3e26f66a9d1843564df0a7a6b
Krasimir Georgiev <support@vip-consult.co.uk>
2015-10-21 11:28:16 +0100

    use the oficial postgresql image and extend it to change the locale

--- start_dev.sh
+++ start_dev.sh
@@ -12,6 +12,8 @@ cn="mysql1"
 if [ "$1" == "" ] || [ $1 == $cn ] ;then
 echo "Starting $cn"
 	docker run --restart=always -d --name $cn \
+		--log-driver=syslog \
+		--log-opt syslog-tag=$cn \
 		-p 127.0.0.1:3306:3306 \
                 -v /etc/localtime:/etc/localtime:ro \
                 -v /etc/timezone:/etc/timezone:ro \
@@ -25,11 +27,13 @@ cn="psql1"
 if [ "$1" == "" ] || [ $1 == $cn ] ;then
 echo "Starting $cn"
 	docker run --restart=always -d --name $cn \
+		--log-driver=syslog \
+                --log-opt syslog-tag=$cn \
 		-p 127.0.0.1:5432:5432 \
                 -v /etc/localtime:/etc/localtime:ro \
                 -v /etc/timezone:/etc/timezone:ro \
 		-v /home/postgresql:/var/lib/postgresql/data \
-		postgres:9.3
+		vipconsult/postgres
 		#-e PG_LOCALE="en_GB.UTF-8 UTF-8" \
 		#vipconsult/pgsql93
 fi
@@ -38,6 +42,8 @@ cn="smtp"
 if [ "$1" == "" ] || [ $1 == $cn ] ;then
 echo "Starting $cn"
 	docker run --restart=always -d --name $cn \
+		--log-driver=syslog \
+                --log-opt syslog-tag=$cn \
 		-h dev.vip-consult.co.uk \
 		-p 127.0.0.1:25:25 \
                 -v /etc/localtime:/etc/localtime:ro \
@@ -49,6 +55,8 @@ cn="php53"
 if [ "$1" == "" ] || [ $1 == $cn ] ;then
 echo "Starting $cn"
 	docker run --restart=always -d --name $cn \
+		--log-driver=syslog \
+                --log-opt syslog-tag=$cn \
 		-v /var/run:/var/run \
 		-v /home/http:/home/http  \
                 -v /etc/localtime:/etc/localtime:ro \
@@ -65,6 +73,8 @@ cn="php"
 if [ "$1" == "" ] || [ $1 == $cn ] ;then
 echo "Starting $cn"
 	docker run --restart=always -d  --name $cn \
+		--log-driver=syslog \
+                --log-opt syslog-tag=$cn \
 		-v /var/run:/var/run \
 		-v /home/http:/home/http  \
                 -v /etc/localtime:/etc/localtime:ro \
@@ -81,6 +91,8 @@ cn="nginx"
 if [ "$1" == "" ] || [ $1 == $cn ] ;then
 echo "Starting $cn"
 	docker run --restart=always -d --name $cn  \
+		--log-driver=syslog \
+                --log-opt syslog-tag=$cn \
 		-v /home/http:/home/http \
 		-v /var/run:/var/run \
                 -v /etc/localtime:/etc/localtime:ro \
@@ -93,6 +105,8 @@ cn="fs"
 if [ "$1" == "" ] || [ $1 == $cn ] ;then
 echo "Starting $cn"
         sudo docker run --restart=always -d --name $cn \
+		--log-driver=syslog \
+                --log-opt syslog-tag=$cn \
                 --privileged \
 		-v /etc/localtime:/etc/localtime:ro \
                 -v /etc/timezone:/etc/timezone:ro \
@@ -108,25 +122,18 @@ cn="backend"
 if [ "$1" == "" ] || [ $1 == $cn ] ;then
 echo "Starting $cn"
 	docker run -it --rm --name $cn \
-	--link psql1:psql1 \
-	-P \
-	-v $(which docker):/docker \
-	-e "smtpServer=smtp" \
-	-e "portalIP=192.168.168.168" \
-	--link smtp:smtp \
-	-v /var/run/docker.sock:/docker.sock \
-	-v /home/golang/src/github.com/vipconsult/portalBackend:/go/src/portalBackend  \
-	b
+		--log-driver=syslog \
+	        --log-opt syslog-tag=$cn \
+		--link psql1:psql1 \
+		-P \
+		-v $(which docker):/docker \
+		-e "smtpServer=smtp" \
+		-e "portalIP=192.168.168.168" \
+		--link smtp:smtp \
+		-v /var/run/docker.sock:/docker.sock \
+		-v /home/golang/src/github.com/vipconsult/portalBackend:/go/src/portalBackend  \
+		b
 fi
-#cn="logrotate"
-#if [ "$1" == "" ] || [ $1 == $cn ] ;then
-#echo "Starting $cn"
-#	docker run --restart=always -d --name $cn \
-#        	-v /var/lib/docker:/var/lib/docker \
-#                -v /etc/localtime:/etc/localtime:ro \
-#                -v /etc/timezone:/etc/timezone:ro \
-#        	vipconsult/logrotate
-#fi
 
 #cn="data"
 #if [ "$1" == "" ] || [ $1 == $cn ] ;then
@@ -139,8 +146,9 @@ fi
 cn="samba"
 if [ "$1" == "" ] || [ $1 == $cn ] ;then
 echo "Starting $cn"
-        docker run \
-		--restart=always -d --name $cn \
+        docker run --restart=always -d --name $cn \
+		--log-driver=syslog \
+                --log-opt syslog-tag=$cn \
 		-v /etc/localtime:/etc/localtime:ro \
                 -v /etc/timezone:/etc/timezone:ro \
 		-p 139:139 -p 445:445  \
