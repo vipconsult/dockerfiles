fc4ac47e5622038f3a38b4e5cbf470508dd2c759
Krasimir Georgiev <support@vip-consult.co.uk>
2015-11-30 16:22:55 +0000

    add support for sessoind saved in the memcached, including a load balancing server

--- php53/start.sh
+++ php53/start.sh
@@ -79,6 +79,16 @@ if [ -n "${PHP53_group}" ]; then
 else
     sed -i -e "s/.*group =.*/group = nogroup/" $fpmFile
 fi
+if [ -n "${PHP53_save_handler}" ]; then
+    sed -i -e "s/.*session.save_handler =.*/session.save_handler = $PHP53_save_handler/" $iniFile
+    
+    ## if it has a load balancing server it needs to save the sessions on both in case the client hits different servers on another request
+    if [ -n "${LB_SERVER}" ]; then
+        sed -i -e "s/.*session.save_path =.*/session.save_path = tcp:\/\/$MEMCACHED_SERVER:11211?persistent=1&weight=1&timeout=1&retry_interval=15,tcp:\/\/$LB_SERVER:11211?persistent=1&weight=1&timeout=1&retry_interval=15/" $iniFile
+    else
+        sed -i -e "s/.*session.save_path =.*/session.save_path = tcp:\/\/$MEMCACHED_SERVER:11211?persistent=1&weight=1&timeout=1&retry_interval=15/" $iniFile
+    fi
+fi
 
 ## process manager settings
 
