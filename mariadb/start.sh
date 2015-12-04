837f260d3987b8694f4401851782ac6b672c73d3
Krasimir Georgiev <support@vip-consult.co.uk>
2015-12-04 18:57:37 +0000

    galera should be ok , commiting to test the lb server

--- mariadb/start.sh
+++
@@ -1,5 +0,0 @@
-if [ -n "${MYSQL_ROOT_PASSWORD}" ]; then
-    mysqld_safe --skip-grant-tables --skip-networking &
-    mysqladmin -u root password $MYSQL_ROOT_PASSWORD
-fi
-mysqld --wsrep_cluster_address=gcomm://$LB_SERVER
