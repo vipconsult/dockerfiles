45fff56f4a886cc7ed610169b08179a5dfec299b
krasi georgiev <krasi@vip-consult.co.uk>
2015-12-22 13:48:44 +0200

    fix the headers var for the dkim signature

--- smtp/entrypoint.sh
+++ smtp/entrypoint.sh
@@ -22,12 +22,11 @@ grep -q "remote_max_parallel=$SMTP_REMOTE_MAX_PARALLEL" /etc/exim4/exim4.conf.te
         sed -i "/^.*\/usr\/sbin\/exim4/ a queue_run_max=$SMTP_QUEUE_RUN_MAX" /etc/exim4/exim4.conf.template
     fi
 
-    sed -i -e "s/domainlist local_domains = MAIN_LOCAL_DOMAINS/domainlist local_domains =/g" /etc/exim4/exim4.conf.template  && \
-    sed -i '/^.*\/usr\/sbin\/exim4/ a DKIM_CANON = relaxed' /etc/exim4/exim4.conf.template && \
-    sed -i '/^.*\/usr\/sbin\/exim4/ a DKIM_SELECTOR = default' /etc/exim4/exim4.conf.template && \
-    sed -i '/^.*\/usr\/sbin\/exim4/ a DKIM_PRIVATE_KEY = /var/spool/exim4/dkim.main.key' /etc/exim4/exim4.conf.template && \
-    sed -i '/^.*\/usr\/sbin\/exim4/ a DKIM_DOMAIN = ${lc:${domain:$h_from:}}' /etc/exim4/exim4.conf.template && \
-    sed -i '/^.*\/usr\/sbin\/exim4/ a DKIM_SIGN_HEADERS = true' /etc/exim4/exim4.conf.template
+    sed -i -e "s/domainlist local_domains = MAIN_LOCAL_DOMAINS/domainlist local_domains =/g" /etc/exim4/exim4.conf.template  
+    sed -i '/^.*\/usr\/sbin\/exim4/ a DKIM_CANON = relaxed' /etc/exim4/exim4.conf.template 
+    sed -i '/^.*\/usr\/sbin\/exim4/ a DKIM_SELECTOR = default' /etc/exim4/exim4.conf.template 
+    sed -i '/^.*\/usr\/sbin\/exim4/ a DKIM_PRIVATE_KEY = /var/spool/exim4/dkim.main.key' /etc/exim4/exim4.conf.template 
+    sed -i '/^.*\/usr\/sbin\/exim4/ a DKIM_DOMAIN = ${lc:${domain:$h_from:}}' /etc/exim4/exim4.conf.template
     sed -i "/^.*\/usr\/sbin\/exim4/ a REMOTE_SMTP_HELO_DATA=$DOMAINNAME" /etc/exim4/exim4.conf.template
     ## this will add missing headers to the outgoing messages
     sed -i "/^.*acl_check_rcpt:/ a accept control = submission/sender_retain" /etc/exim4/exim4.conf.template
