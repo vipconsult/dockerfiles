010a954416d0f7362d39402c3876284a067b28fa
krasi georgiev <krasi@vip-consult.co.uk>
2017-01-11 00:08:47 +0000

    added the enw nfs-moby-mount image

---
+++ nfs-moby-mount/start.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+# get the shell envs
+set -e
+
+# check the the required vars are set
+if [ -z "$SERVER" ]; then
+    echo "SERVER var not set"
+    exit 1
+fi
+
+if [ -z "$MOUNT" ]; then
+    echo "MOUNT var not set"
+    exit 1
+fi
+
+
+# use nsenter to enter the docker host so we can mount the amazon EFS share in the host and all containers will have access to the mounted folder
+# install required packages and mount the NFS share using env vars $SERVER and  $MOUNT
+# create the $MOUNT dir if it doesn't exist
+# and finally run inotifywait which will output all file folder and file operations so we have some basic logging
+nsenter -t 1 -m -u -i -n sh -c "apk update && apk add nfs-utils inotify-tools && mkdir -p $MOUNT && mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $SERVER $MOUNT && inotifywait -m $MOUNT"
