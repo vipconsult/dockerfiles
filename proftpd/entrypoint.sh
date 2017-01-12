48b0552f3a9ea18a9f580b91ad654d188c5f5f1c
Stan Velikov <stan@vip-consult.solutions>
2017-01-12 10:10:40 -0500

    proftp - allow root login file permissions, add percona submodule

--- proftpd/entrypoint.sh
+++ proftpd/entrypoint.sh
@@ -3,9 +3,11 @@ set -e
 CONF_FILE="/etc/proftpd/ftpd.passwd"
 
 # in case docker mounted this is a directory
-if [ -d "$CONF_FILE" ]; then rmdir rmdir /etc/proftpd/ftpd.passwd ; fi
+# this fails as docker mounted dirs can't be delted
+#if [ -d "$CONF_FILE" ]; then rmdir /etc/proftpd/ftpd.passwd ; fi
 # in case the file doesn't exist
-touch $CONF_FILE
+#touch $CONF_FILE
+
 chown root $CONF_FILE
 chmod 660  $CONF_FILE
 
